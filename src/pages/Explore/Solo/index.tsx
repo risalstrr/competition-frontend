e7087cf8364d6ab1435f8f9c3086bc46b841ae96
ramantii <ramanti.prajna@gmail.com>
2023-12-22 13:20:41 +0700

    finished tanatoraja  module

--- src/pages/Explore/Solo/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { SoloModule } from "src/components/modules/ExploreModule/solo";
-import type { NextPage } from "next";
-
-const Solo: NextPage = () => <SoloModule />;
-
-export default Solo;
